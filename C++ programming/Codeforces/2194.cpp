12bf6e2ab7fe4516b8972f21f107424c18f46d55
Jeetu Mehra <jeetumehramtr@gmail.com>
2026-03-21 23:39:44 +0530

    codeforces 2194 Q2,Q4 and 2195 Q1

--- C++ programming/Codeforces/2194.cpp
+++ C++ programming/Codeforces/2194.cpp
@@ -1,15 +1,98 @@
 // 2194A Lawn Mower
 
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int T;
+//     cin>>T;
+//     while(T--){
+//         int n,w;
+//         cin>>n>>w;
+//         int ans=(n/w)*(w-1)+(n%w);
+//         cout<<ans<<endl;        
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
+// 2194B Offshores
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int T;
+//     cin>>T;
+//     while(T--){
+//         long long n,x,y;
+//         cin>>n>>x>>y;
+//         vector<long long>a(n),b(n);
+//         long long t=0;
+//         for(int i=0;i<n;i++) {
+//             cin>>a[i];
+//             b[i]=a[i]/x;
+//             t+=b[i];
+//         }
+//         long long ans=0;
+//         for(int i=0;i<n;i++){
+//             long long r=1LL*(t-b[i])*y+a[i];
+//             ans=max(ans,r);
+//         }
+//         cout<<ans<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
+
+// 2194D Table Cut
+// there are 2 parts of question, 1st part is solved and 2nd part is unsolved(the commented one)
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
     int T;
     cin>>T;
     while(T--){
-        int n,w;
-        cin>>n>>w;
-        int ans=(n/w)*(w-1)+(n%w);
-        cout<<ans<<endl;        
+        int n,m;
+        cin>>n>>m;
+        vector<vector<int>>a(n,vector<int>(m));
+        int c1=0;
+        vector<int>v;
+        for(int i=0;i<n;i++){
+            int c=0;
+            for(int j=0;j<m;j++){
+                cin>>a[i][j];
+                if(a[i][j]) {
+                    c1++;
+                    c++;
+                }
+                v.push_back(c);
+            }
+        }
+        int x=c1/2;
+        if(c1%2) cout<<(x)*(x+1)<<endl;
+        else cout<<(x)*(x)<<endl;  
+        // string ans="";
+        // int i=0;
+        // while(true){
+        //     while(x>v[i]){
+        //         ans+='D';
+        //         x-=v[i];
+        //         i++;
+        //     }
+        //     while(c1/2){
+        //         ans+='R';
+        //         if()
+        //     }
+        // }
+
     }
     return 0;
 }
---
+++ C++ programming/Codeforces/2195.cpp
@@ -0,0 +1,21 @@
+// 2195A Sieve of Erato67henes
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        bool x=false,y=false;
+        for(int i=0;i<n;i++) {
+            cin>>a[i];
+            if(a[i]==1) x=true;
+            if(a[i]==67) y=true;
+        }
+        if(y) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
