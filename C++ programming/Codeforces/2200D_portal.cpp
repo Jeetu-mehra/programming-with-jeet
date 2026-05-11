80754afbf377a29c163c817b723b13aeaacfb6f9
Jeetu Mehra <jeetumehramtr@gmail.com>
2026-05-11 23:51:46 +0530

    2202A

---
+++ C++ programming/Codeforces/2200D_portal.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n,x,y;
+        cin>>n>>x>>y;
+        vector<int> p(n);
+        for(int i=0;i<n;i++){
+            cin>>p[i];
+        }
+        
+    }
+    return 0;
+}
