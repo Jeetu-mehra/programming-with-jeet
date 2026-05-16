6ae3b39a3f3a3d35e855c74cf479572ea1932d91
Jeetu Mehra <jeetumehramtr@gmail.com>
2026-05-16 23:59:25 +0530

    notes api basic

--- Backend/2.Notes API/src/app.js
+++ Backend/2.Notes API/src/app.js
@@ -1,7 +1,27 @@
 //server ko create krna
 const express = require('express');
 
-
 const app = express();
+app.use(express.json()); // Middleware to parse JSON bodies
+
+const notes = []; // In-memory array to store notes
+
+
+app.post('/notes', (req, res) => {
+    notes.push(req.body); // Add the new note to the array
+    res.status(201).json({ 
+        message: 'Note created successfully' 
+    }); // Send a response back to the client
+
+})
+
+app.get('/notes', (req, res) => {
+    res.status(200).json({
+        message: 'Notes retrieved successfully',
+        notes: notes
+    }); // Send the array of notes as a JSON response
+})
+
+
 
 module.exports = app;
