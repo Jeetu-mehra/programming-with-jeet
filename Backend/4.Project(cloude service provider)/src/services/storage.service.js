44493cae6c48ae2d4fb879a89ac1e95a5a4b7e62
Jeetu Mehra <jeetumehramtr@gmail.com>
2026-05-19 23:52:04 +0530

    cloude service backend

---
+++ Backend/4.Project(cloude service provider)/src/services/storage.service.js
@@ -0,0 +1,23 @@
+const {ImageKit} = require("@imagekit/nodejs")
+
+
+const imagekit = new ImageKit({
+    privatekey : process.env.IMAGEKIT_PRIVATE_KEY
+
+})
+
+
+
+async function uploadFile(buffer) {
+
+    console.log(buffer);
+
+    const result = await imagekit.files.upload({
+        file : buffer.toString('base64'),
+        fileName : "image.jpg"
+    })
+
+    return result;
+}
+
+module.exports = uploadFile;
