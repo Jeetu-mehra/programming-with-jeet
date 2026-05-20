97075f20e77844df105402f625cf554f599f6d5c
Jeetu Mehra <jeetumehramtr@gmail.com>
2026-05-20 23:34:29 +0530

    frontend set up

---
+++ Backend/4.Project(cloude service provider)/Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
